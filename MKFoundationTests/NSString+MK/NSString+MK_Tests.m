b67d3b3ed7dcf85880da8c8ed6bfc6514fbe6719
Michal Konturek <michal.konturek@gmail.com>
2013-10-23 19:28:49 +0100

    NSString+MK passes tests.

---
+++ MKFoundationTests/NSString+MK/NSString+MK_Tests.m
@@ -0,0 +1,37 @@
+//
+//  NSString+MK_Tests.m
+//  MKFoundation
+//
+//  Created by Michal Konturek on 23/10/2013.
+//  Copyright (c) 2013 Michal Konturek. All rights reserved.
+//
+
+#import "BaseTest.h"
+
+@interface NSString_MK_Tests : SenTestCase
+
+@end
+
+@implementation NSString_MK_Tests
+
+- (void)test_isStringEmptyOrNil_returns_true {
+    BOOL result = [NSString isStringEmptyOrNil:@""];
+    assertThatBool(result, equalToBool(YES));
+}
+
+- (void)test_isStringEmptyOrNil_returns_true_when_spaces {
+    BOOL result = [NSString isStringEmptyOrNil:@"   "];
+    assertThatBool(result, equalToBool(YES));
+}
+
+- (void)test_isStringEmptyOrNil_returns_true_when_nil {
+    BOOL result = [NSString isStringEmptyOrNil:nil];
+    assertThatBool(result, equalToBool(YES));
+}
+
+- (void)test_isStringEmptyOrNil_returns_false {
+    BOOL result = [NSString isStringEmptyOrNil:@"A B C D"];
+    assertThatBool(result, equalToBool(NO));
+}
+
+@end
---
+++ MKFoundationTests/NSString+MK/NSString+MK_UTF_Tests.m
@@ -0,0 +1,25 @@
+//
+//  NSString+MK_UTF_Tests.m
+//  MKFoundation
+//
+//  Created by Michal Konturek on 23/10/2013.
+//  Copyright (c) 2013 Michal Konturek. All rights reserved.
+//
+
+#import "BaseTest.h"
+
+@interface NSString_MK_UTF_Tests : SenTestCase
+
+@end
+
+@implementation NSString_MK_UTF_Tests
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+@end
