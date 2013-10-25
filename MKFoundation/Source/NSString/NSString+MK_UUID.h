78098f6d460bea49ee1900f7462df4845b77820c
Michal Konturek <michal.konturek@gmail.com>
2013-10-25 17:21:33 +0100

    Added NSString+MK_UUID

---
+++ MKFoundation/Source/NSString/NSString+MK_UUID.h
@@ -0,0 +1,13 @@
+//
+//  NSString+MK_UUID.h
+//  MKFoundation
+//
+//  Created by Michal Konturek on 25/10/2013.
+//  Copyright (c) 2013 Michal Konturek. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (MK_UUID)
+
+@end
