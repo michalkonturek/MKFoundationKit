1ff7fb39b4c81eaacea399b1bd11f6c4e4ee596d
Michal Konturek <michal.konturek@gmail.com>
2013-12-15 12:07:35 +0000

    .

--- MKFoundationKit/Source/NSNumber/NSNumber+MK_Comparison.h
+++ MKFoundationKit/Source/NSNumber/NSNumber+MK_Comparison.h
@@ -10,14 +10,6 @@
 
 @interface NSNumber (MK_Comparison)
 
-+ (instancetype)mk_max:(NSArray *)numbers;
-
-+ (instancetype)mk_min:(NSArray *)numbers;
-
-- (instancetype)mk_max:(NSArray *)others;
-
-- (instancetype)mk_min:(NSArray *)others;
-
 - (BOOL)mk_isTheSame:(NSNumber *)other;
 
 - (BOOL)mk_isGreaterThan:(NSNumber *)other;
